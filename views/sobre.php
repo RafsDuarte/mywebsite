b9930b9bda2d0004cc25c65925c28dea7e3ef5a7
RafsDuarte <rafinha.lduarte@hotmail.com>
2021-06-12 17:35:56 -0300

    update

--- views/sobre.php
+++ views/sobre.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3">
-				<img src="app/img/j4.jpg" class="img-thumbnail img-responsive center" >
+				<img src="#" class="img-thumbnail img-responsive center" >
 				<p>
 					O site foi desenvolvido por Rafael Felipe Leite Duarte. Bacharel em Sistemas de Informação na Associação Educacional Dom Bosco. Reside em Resende, interior do Rio de Janeiro. Este projeto é o primeiro desenvolvido utilizando a linguagem PHP.
 				</p>
